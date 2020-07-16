879dd542041892d65aa19846a974e3475ec4d44c
Jérémy Godde <54586623+JeremyGodde@users.noreply.github.com>
2020-07-16 21:37:27 +0200

    correction

--- js/editor.js
+++ js/editor.js
@@ -45,7 +45,8 @@ function View(article,view)
 			{
 				contenu = contenu.slice(1,contenu.length);
 			}
-		
+
+
 		//détermination de la première balise
 			if(contenu.search("'>")==0){
 				contenu = contenu.replace("'>","<p class=\"important\">");
@@ -58,22 +59,26 @@ function View(article,view)
 			}
 
 		//italique et gras
+
 			contenu = contenu.replace(/\*\/>/gi,"<strong>italiq>");
-			contenu = contenu.replace(/\\/*>/gi,"<strong>italiq>");
+			contenu = contenu.replace(/\/\*>/gi,"<strong>italiq>");
 
 			contenu = contenu.replace(/\/>/gi,"italiq>");
 			
-			contenu = contenu.replace(/<\/*/gi,"</em></strong>");
+			contenu = contenu.replace(/<\/\*/gi,"double>");
 			contenu = contenu.replace(/<\//gi,"</em>");
+			contenu = contenu.replace(/double>/gi,"</em></strong>");
 
 			contenu = contenu.replace(/<\*\//gi,"</em></strong>");
 			
 			contenu = contenu.replace(/\*>/gi,"<strong>");
 			contenu = contenu.replace(/<\*/gi,"</strong>");
 
+
 		//barre
-			contenu = contenu.replace(/b>/gi,"<span style=\"text-decoration:line-through;\">");
+			contenu = contenu.replace(/b>/gi,"<span style=\"text-decoration:line-through var(--theme);\">"); // class=\"barre\"
 			contenu = contenu.replace(/<b/gi,"</span>");
+
 
 		//insertion d'image DEPRECATED
 			contenu = contenu.replace(/\n!>small<size/gi,"</br><img class=\"center\" src=\"");
@@ -105,7 +110,8 @@ function View(article,view)
 			contenu = contenu.replace(/!> medium <size/gi,"<img class=\"center\" src=\"");
 			contenu = contenu.replace(/!>medium <size/gi,"<img class=\"center\" src=\"");
 			contenu = contenu.replace(/!> medium<size/gi,"<img class=\"center\" src=\"");
-			
+
+
 		//insertion d'image	
 			contenu = contenu.replace(/!>left<size/gi,"<img class=\"left\" src=\"");
 			contenu = contenu.replace(/!> left <size/gi,"<img class=\"left\" src=\"");
@@ -129,28 +135,32 @@ function View(article,view)
 			
 			contenu = contenu.replace(/!>/gi,"<img src=\"");
 			contenu = contenu.replace(/<!/gi,"\" alt=\"image non disponible\"/>");
-		
+
+
 		//insertion d'articles
 			contenu = contenu.replace(/a>/gi,"article>");
 			contenu = contenu.replace(/<a/gi,"\"><img src=\"https://i.imgur.com/gkWVdWs.png\" width=\"100%\"/><span class=\"overlay\"><span>Article 195</span><span>Le Fil 999</span></span></a>");
 			contenu = contenu.replace(/article>/gi,"<a class=\"article\" href=\"");	
+
 
 		//découpage des note de bas de page
 			var p = contenu.search(/\^>/);
 			while(p>=0){
 				var note = contenu.substr(p+2,contenu.search(/<\^/)-p-2);
 				notebaspage++;
-				contenu = contenu.replace("^>"+note+"<^"," <a class=\"intern\" style=\"font-size:60%;vertical-align:super;\" href=\"#note_"+notebaspage+"\"> ["+notebaspage+"] </a>");
+				contenu = contenu.replace("^>"+note+"<^"," <a class=\"intern\" style=\"font-size:60%;vertical-align:super;\" href=\"#note_"+notebaspage+"\"> ["+notebaspage+"] </a>"); // class=\"note\"
 				baspage += "\nsmp;note_"+notebaspage+"\">["+notebaspage+"] "+note;
 				p = contenu.search(/\^>/);
 			}
 
 			contenu += baspage;
-	
+
+
 		// mise en forme des notes de bas de page
 			contenu = contenu.replace(/<'\nsmp;/g,"</p><p style=\"font-size:60%\" class=\"my-0\" id=\"");
 			contenu = contenu.replace(/<t\nsmp;/g,"</h2><p style=\"font-size:60%\" class=\"my-0\" id=\"");
-			contenu = contenu.replace(/\nsmp;/g,"</p><p style=\"font-size:60%\" class=\"my-0\" id=\"");
+			contenu = contenu.replace(/\nsmp;/g,"</p><p style=\"font-size:60%\" class=\"my-0\" id=\""); // class=\"note-pied\"
+
 
 		//mode d'édition
 			p = contenu.search(/e>/);
@@ -161,25 +171,30 @@ function View(article,view)
 				contenu = contenu.substr(0,p) + edit + contenu.substr(p_fin+2);
 				p = contenu.search(/e>/);
 			}
-		
+
+
 		//fin de la procédure d'échappement de l'italique
 			contenu = contenu.replace(/italiq>/gi,"<em>");
-			
+
+
 		//lien
 			contenu = contenu.replace(/l>/gi,"<a href=\"");
 			contenu = contenu.replace(/<:>/gi,"\">");
 			contenu = contenu.replace(/<l/gi,"</a>");
-		
+
+
 		//titre
 			contenu = contenu.replace(/t>/gi,"</p><h2>");
 			contenu = contenu.replace(/<t/gi,"</h2><p>");
-			
+
+
 		//citation
 			contenu = contenu.replace(/\n\'>/gi,"</p><p class=\"important\">");
 			contenu = contenu.replace(/<\'\n/gi,"</p><p>");
 
 			contenu = contenu.replace(/\'>/gi,"<span class=\"important\">");
 			contenu = contenu.replace(/<\'/gi,"</span>");
+
 
 		//liste
 			contenu = contenu.replace(/\.>\n/gi,"</p><ul><li>");
@@ -199,19 +214,24 @@ function View(article,view)
 			contenu = contenu.replace(/\n<\./gi,"</li></ul><p>");
 			contenu = contenu.replace(/<\./gi,"</li></ul><p>");
 
+
 		//paragraphe simple
 			contenu = contenu.replace(/\n/gi,"</p><p>");
+
 
 		//insertion des esperluettes
 			contenu = contenu.replace(/&/gi,"ₔ");
 
+
 		//fermeture des dernières balises
 			contenu = "<section>"+contenu+"</p></section>";
-		
+
+
 		//nettoyage des balises vides
 			contenu = contenu.replace(/<p><\/p>/gi,"");
 			contenu = contenu.replace(/<li><\/li>/gi,"");
-			
+
+
 		//insertion de l'article
 			view.innerHTML = contenu;
 }
